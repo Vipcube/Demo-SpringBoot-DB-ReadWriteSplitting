cdc537af93e2c4db9e8e4c276688c2a62272f793
Brad Chen <brad.chen@rybit.io>
2023-04-20 17:22:39 +0800

    Apply the proxy config

--- src/main/java/org/vipcube/spring/demo/entity/AppUser.java
+++ src/main/java/org/vipcube/spring/demo/entity/AppUser.java
@@ -17,7 +17,7 @@ import javax.persistence.Id;
 @NoArgsConstructor
 public class AppUser {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
 }
