9bb06baf58e48ab0b75dd5f5254c7f6e12107365
Brad Chen <brad.chen@rybit.io>
2023-05-09 00:22:27 +0800

    transactional read-only

--- src/main/java/org/vipcube/spring/demo/service/impl/AppUserServiceImpl.java
+++ src/main/java/org/vipcube/spring/demo/service/impl/AppUserServiceImpl.java
@@ -2,6 +2,7 @@ package org.vipcube.spring.demo.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.vipcube.spring.demo.entity.AppUser;
 import org.vipcube.spring.demo.repository.AppUserRepository;
 import org.vipcube.spring.demo.service.AppUserService;
@@ -14,11 +15,13 @@ public class AppUserServiceImpl implements AppUserService {
     private final AppUserRepository repository;
 
     @Override
+    @Transactional(readOnly = true)
     public List<AppUser> findAll() {
         return this.repository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public AppUser getById(long id) {
         return this.repository.findById(id).orElse(null);
     }
